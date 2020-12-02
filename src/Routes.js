e6c110e77e99793e7017d4b21b76d2d7521718f0
TonyK <sunghk0901@gmail.com>
2020-12-02 20:36:23 +0900

    Modify: Router.js 설정 추가

--- src/Routes.js
+++ src/Routes.js
@@ -6,7 +6,17 @@ class Routes extends React.Component {
     return (
       <Router>
         <Switch>
-          <h1>양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님양준식멘토님</h1>
+          <Route exact path="/LoginLee" component={LoginLee} />
+          <Route exact path="/LoginPark" component={LoginPark} />
+          <Route exact path="/LoginKim" component={LoginKim} />
+          <Route exact path="/LoginLim" component={LoginLim} />
+          <Route exact path="/LoginIm" component={LoginIm} />
+
+          <Route exact path="/MainLee" component={MainLee} />
+          <Route exact path="/MainPark" component={MainPark} />
+          <Route exact path="/MainKim" component={MainKim} />
+          <Route exact path="/MainLim" component={MainLim} />
+          <Route exact path="/MainIm" component={MainIm} />
         </Switch>
       </Router>
     )
