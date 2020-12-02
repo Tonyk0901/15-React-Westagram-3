ef61cac0bd0ca896c1b72acb32d2a956caa876c8
TonyK <sunghk0901@gmail.com>
2020-12-02 20:50:37 +0900

    Modify: 초기셋팅 추가추가추가

---
+++ src/Pages/chaehoonpark/Login/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class LoginPark extends React.Component {
+    render() {
+        return (
+            <div>haha</div>
+        )
+    }
+}
+
+export default LoginPark;
