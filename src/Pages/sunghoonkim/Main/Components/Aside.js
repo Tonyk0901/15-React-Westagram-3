2d8f0d6b7f848071279949999543eccd465f94e6
TonyK <sunghk0901@gmail.com>
2020-12-11 21:09:13 +0900

    Modify: - Aside 컴포넌트에 fixed 포지션 추가 - Aside 컴포넌트를 미디어 쿼리를 이용하여 반응형으로 수정정 - Aside 컴포넌트의 스토리 / 추천 부분 높이 수정 및 스크롤 추가

--- src/Pages/sunghoonkim/Main/Components/Aside.js
+++ src/Pages/sunghoonkim/Main/Components/Aside.js
@@ -8,70 +8,72 @@ class Aside extends React.Component {
     const { STORY_DATA, RECOMMEND_DATA, FOOTER_CARDS } = ASIDE_DATA;
     return (
       <div className="AsideKim">
-        <section className="profiles">
-          <img src="images/sunghoonkim/iuImageSample2.jpg" alt="IU" />
-          <div className="column">
-            <div className="strong-font">sunghoon_kim</div>
-            <div className="description light-font">징쨩 | 커여웡</div>
-          </div>
-        </section>
-        <section className="stories">
-          <header>
-            <div className="light-font">스토리</div>
-            <div className="strong-font add-cursor">모두 보기</div>
-          </header>
-          <section>
-            {STORY_DATA.map(({ userId, timeStamp, profilePicture }, index) => {
-              return (
-                <div key={index} className="body">
-                  <img src={profilePicture} alt="IU" />
-                  <div className="column">
-                    <div className="strong-font">{userId}</div>
-                    <div className="timetag light-font">{timeStamp}</div>
-                  </div>
-                </div>
-              )
-            })}
+        <div className="wrapper">
+          <section className="profiles">
+            <img src="images/sunghoonkim/iuImageSample2.jpg" alt="IU" />
+            <div className="column">
+              <div className="strong-font">sunghoon_kim</div>
+              <div className="description light-font">징쨩 | 커여웡</div>
+            </div>
           </section>
-        </section>
-        <section className="recommends">
-          <header>
-            <div className="light-font">회원님을 위한 추천</div>
-            <div className="strong-font add-cursor">모두 보기</div>
-          </header>
-          <section>
-            {RECOMMEND_DATA.map(({ userId, profilePicture, description }, index) => {
-              return (
-                <div key={index} className="body">
-                  <div className="bigcolumn">
+          <section className="stories">
+            <header>
+              <div className="light-font">스토리</div>
+              <div className="strong-font add-cursor">모두 보기</div>
+            </header>
+            <section>
+              {STORY_DATA.map(({ userId, timeStamp, profilePicture }, index) => {
+                return (
+                  <div key={index} className="body">
                     <img src={profilePicture} alt="IU" />
                     <div className="column">
                       <div className="strong-font">{userId}</div>
-                      <div className="timetag light-font">{description}</div>
+                      <div className="timetag light-font">{timeStamp}</div>
                     </div>
                   </div>
-                  <div className="bigcolumn">
-                    <Link to="/MainKim">팔로우</Link>
-                  </div>
-                </div>
-              )
-            })}
+                )
+              })}
+            </section>
           </section>
-        </section>
-        <footer className="footers">
-          <ul>
-            {FOOTER_CARDS.map((footerCard, index) => {
-              return (
-                <li key={index}>
-                  <Link to="/MainKim" className="light-font delete-hyper-link">
-                    {footerCard}
-                  </Link>
-                </li>
-              )
-            })}
-          </ul>
-          <span className="light-font">© 2019 INSTAGRAM</span>
-        </footer>
+          <section className="recommends">
+            <header>
+              <div className="light-font">회원님을 위한 추천</div>
+              <div className="strong-font add-cursor">모두 보기</div>
+            </header>
+            <section>
+              {RECOMMEND_DATA.map(({ userId, profilePicture, description }, index) => {
+                return (
+                  <div key={index} className="body">
+                    <div className="bigcolumn">
+                      <img src={profilePicture} alt="IU" />
+                      <div className="column">
+                        <div className="strong-font">{userId}</div>
+                        <div className="timetag light-font">{description}</div>
+                      </div>
+                    </div>
+                    <div className="bigcolumn">
+                      <Link to="/MainKim">팔로우</Link>
+                    </div>
+                  </div>
+                )
+              })}
+            </section>
+          </section>
+          <footer className="footers">
+            <ul>
+              {FOOTER_CARDS.map((footerCard, index) => {
+                return (
+                  <li key={index}>
+                    <Link to="/MainKim" className="light-font delete-hyper-link">
+                      {footerCard}
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+            <span className="light-font">© 2019 INSTAGRAM</span>
+          </footer>
+        </div>
       </div >
     );
   }
